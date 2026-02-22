63097a0a0a00cc40890fb2acf5deab056b1b7c25
jeevan4476 <jeevanr2431@gmail.com>
2026-02-22 19:11:23 +0530

    feat: redis support with lua scripts

---
+++ src/persistence/redis_reporistory.ts
@@ -0,0 +1,2 @@
+//similar interface in regards of the postgresSql repository
+//redis lua scripts to mimic postgresSQL transactions for atomicity and isolation properties.
---
+++ src/persistence/scripts/acquireToken.lua
@@ -0,0 +1,69 @@
+local bucketKey = KEYS[1]
+local idKey = KEYS[2]
+local capacity = tonumber(ARGV[1])
+local refillRate = tonumber(ARGV[2])
+local now = tonumber(ARGV[3])
+local requested = tonumber(ARGV[4])
+
+-- checking idempotency
+if redis.call("EXISTS", idKey) == 1 then
+    return redis.call("HGETALL",idKey)
+end
+
+-- loading buket 
+local bucket = redis.call("HGETALL",bucketKey)
+
+local tokens
+local lastRefill
+
+if next(bucket) == nil then 
+    tokens = capacity
+    lastRefill = now
+else 
+    tokens = tonumber(redis.call("HGET",bucketKey,"tokens"))
+    lastRefill = tonumber(redis.call("HGET",bucketKey,"last_refill_at"))
+end
+
+--refill tokens 
+local delta = math.max(0,now-lastRefill)
+local refill = (delta/1000) * refillRate
+tokens = math.min(capacity,tokens + refill)
+lastRefill = now
+
+local status 
+local waitTime = 0
+
+-- try to acqurie
+if tokens >= requested then 
+    tokens = tokens - requested
+    status = "GRANTED"
+else 
+    status = "REJECTED"
+    waitTime = math.ceil(((requested - tokens) / refillRate) * 1000)
+end
+
+-- save bucket
+redis.call("HMSET", bucketKey,
+    "tokens", tokens,
+    "last_refill_at", lastRefill,
+    "capacity", capacity,
+    "refill_rate", refillRate
+)
+
+--ttl
+redis.call("PEXPIRE", bucketKey, 60000)
+
+-- save results
+redis.call("HMSET", idKey,
+    "status", status,
+    "tokens_remaining", tokens,
+    "wait_time_ms", waitTime
+)
+
+redis.call("PEXPIRE", idKey, 60000)
+
+return {
+    "status", status,
+    "tokens_remaining", tokens,
+    "wait_time_ms", waitTime
+}
