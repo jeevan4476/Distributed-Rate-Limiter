5961bc1f121c8d468e0228eb0a82cf246565db6d
jeevan4476 <jeevanr2431@gmail.com>
2026-03-07 22:21:41 +0530

    fix: proto build issue

--- examples/rust/build.rs
+++ examples/rust/build.rs
@@ -1,7 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
+    let proto_dir = if std::path::Path::new("proto/ratelimit.proto").exists() {
+        "proto"
+    } else {
+        "../../proto"
+    };
+
+    let proto_file = format!("{}/ratelimit.proto", proto_dir);
+
+    tonic_prost_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile_protos(&["proto/ratelimit.proto"], &["proto"])?;
+        .compile_protos(&[proto_file.as_str()], &[proto_dir])?;
     Ok(())
 }
